1bb05ec00150e2597d3dc8ef49240d02a1f7a3ee
Brian Lynnerup <brian.lynnerup.pedersen@gmail.com>
2016-04-19 14:25:23 +0000

    Added a little project that used npm

---
+++ IntroToNpm/MyShop/listProducts.js
@@ -0,0 +1,14 @@
+var faker = require("faker");
+
+var products = [];
+var prices = [];
+
+for (var i = 0; i < 10; i++){
+    products.push(faker.commerce.productName());
+    prices.push(faker.commerce.price());
+}
+
+console.log("===================\n" + "Welcome to my shop!\n" + "===================");
+for (var i = 0; i < 10; i++){
+    console.log(products[i] + " - $" + prices[i]);
+}
