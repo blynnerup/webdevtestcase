d696e6e659582f56f7598498df849e53dca4781f
Brian Lynnerup <brian.lynnerup.pedersen@gmail.com>
2016-04-20 11:59:53 +0000

    Missed some lines in the last add. Now including the patterns in routing...

--- IntroToExpress/FirstExpressApp/app.js
+++ IntroToExpress/FirstExpressApp/app.js
@@ -4,6 +4,7 @@ var app = express();
 /*
 * Routes ordering matters
 * First route to match a request, triggers and then no other is run!
+* Do not place the CATCH ALL FIRST!
 */
 
 // "/" => "Hi there!;
@@ -20,8 +21,21 @@ app.get("/dog", function(req, res){
    res.send("MEOW!"); 
 });
 
+// routing patterns - : indicates a variable, only valid till the next /
+app.get("/r/:subredditName", function(req, res){
+    console.log(req.params);
+    var subreddit = req.params.subredditName;
+    res.send("WELCOME TO THE " + subreddit.toUpperCase() +" SUBREDDIT!");
+});
+
+app.get("/r/:subredditName/comments/:id/:title/", function(req, res) {
+    console.log(req.params);
+    res.send("Item");
+});
+
+// Catch all routes
 app.get("*", function(req, res){ // * catches (catch all routes) all gets to undefined pages and leads them here..
-   res.send("YOU ARE A STAR!") 
+   res.send("YOU ARE A STAR!");
 });
 
 // Tell Express to listen for requests (start server)
