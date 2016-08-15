381dc78d11cdadda85496b2dc9dce70306735d8d
Brian Lynnerup <brian.lynnerup.pedersen@gmail.com>
2016-08-15 09:40:11 +0000

    Moved the mongoose models to seperate files

--- Associations/reference.js
+++ Associations/reference.js
@@ -1,46 +1,29 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/blog_demo_2");
 
-// Need to define the Post (and in particular postSchema) before the User or mongoose cannot use postSchema in the posts data.
-// Post - title, content
-var postSchema = new mongoose.Schema({
-    title: String,
-    content: String
+// Placement of the mongoose models in seperate files, for cleaner code and files.
+var Post = require("./models/post");
+var User = require("./models/user");
+
+Post.create({
+    title: "How to cook the best burger part 4",
+    content: "BLAM!"
+}, function(err, post){
+  User.findOne({ email: "bob@gmail.com"}, function (err, foundUser){
+      if(err){
+          console.log(err);
+      } else {
+          foundUser.posts.push(post);
+          foundUser.save(function(err, data){
+              if(err){
+                  console.log(err);
+              } else {
+                  console.log(data);
+              }
+          });
+      }
+  });
 });
-
-var Post = mongoose.model("Post", postSchema);
-
-// User - email, name
-var userSchema = new mongoose.Schema({
-    email: String,
-    name: String,
-    posts: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Post"
-    }]
-});
-
-var User = mongoose.model("User", userSchema);
-
-// Post.create({
-//     title: "How to cook the best burger part 3",
-//     content: "iouahnga ifjwan fawfofna"
-// }, function(err, post){
-//   User.findOne({ email: "bob@gmail.com"}, function (err, foundUser){
-//       if(err){
-//           console.log(err);
-//       } else {
-//           foundUser.posts.push(post);
-//           foundUser.save(function(err, data){
-//               if(err){
-//                   console.log(err);
-//               } else {
-//                   console.log(data);
-//               }
-//           });
-//       }
-//   });
-// });
 
 
 // User.create({
@@ -51,10 +34,10 @@ var User = mongoose.model("User", userSchema);
 // Find user 
 // find all posts for that user
 
-User.findOne({email: "bob@gmail.com"}).populate("posts").exec(function(err, user){
-   if(err) {
-       console.log(err);
-   } else {
-       console.log(user);
-   }
-});
+// User.findOne({email: "bob@gmail.com"}).populate("posts").exec(function(err, user){
+//   if(err) {
+//       console.log(err);
+//   } else {
+//       console.log(user);
+//   }
+// });
