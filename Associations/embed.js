ca578dd4fc36a18ac3220908d6ce5f62433f9447
Brian Lynnerup <brian.lynnerup.pedersen@gmail.com>
2016-08-15 09:04:44 +0000

    Working with associations in Mongo

---
+++ Associations/embed.js
@@ -0,0 +1,71 @@
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/blog_demo");
+
+// Need to define the Post (and in particular postSchema) before the User or mongoose cannot use postSchema in the posts data.
+// Post - title, content
+var postSchema = new mongoose.Schema({
+    title: String,
+    content: String
+});
+
+var Post = mongoose.model("Post", postSchema);
+
+// User - email, name
+var userSchema = new mongoose.Schema({
+    email: String,
+    name: String,
+    posts: [postSchema]
+});
+
+var User = mongoose.model("User", userSchema);
+
+
+// var newUser = new User({
+//     email: "hermione@hogwarts.edu",
+//     name: "Hermione Granger"
+// });
+
+// newUser.posts.push({
+//     title: "How to brew polyjuice potion",
+//     content: "Just kidding. Go to postions class to learn it"
+// });
+
+
+// newUser.save(function(err, user){
+//     if(err){
+//         console.log(err);
+//     } else {
+//         console.log(user);
+//     }
+// })
+
+// var newPost = new Post({
+//     title: "Reflections on Apples",
+//     content: "They are delicious"
+// });
+
+// newPost.save(function(err, post){
+//   if(err){
+//       console.log(err);
+//   } else {
+//       console.log(post);
+//   }
+// });
+
+User.findOne({ name: "Hermione Granger"}, function(err, user){
+    if(err){
+        console.log(err);
+    } else {
+        user.posts.push({
+            title: "3 things I really hate",
+            content: "Voldemort. Voldemort. Voldemort"
+        });
+        user.save(function(err, user){
+            if(err){
+                console.log(err);
+            } else {
+                console.log(user);
+            }
+        })
+    }
+});
